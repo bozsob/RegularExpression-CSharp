7d4847692e808bd01e7280f55eb48c30bd21dc18
bozsob <bozso.beatrix@gmail.com>
2017-07-10 13:42:43 +0200

    Convert text file to different encoding type

---
+++ ConvertTextFileToDifferentEncodingType/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConvertTextFileToDifferentEncodingType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(@"C:\boot.ini"))
+                {
+                    StreamWriter sw = new StreamWriter("boot-utf7.txt", false, Encoding.UTF7);
+                    sw.WriteLine(sr.ReadToEnd());
+
+                    sw.Close();
+                    sr.Close();
+                }
+            }
+
+            catch (Exception e)
+            {
+                throw new Exception("Error occured.", e);                
+            }
+        }
+    }
+}
